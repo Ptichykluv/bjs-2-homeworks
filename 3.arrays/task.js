9a29b33612d0ae1a5d89e6d2276dfb0c9f8a397a
Елена Лимонова <glowinggrunt@gmail.com>
2023-07-25 10:45:14 +0300

    Третье задание полностью готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,19 @@
 function compareArrays(arr1, arr2) {
+
+ return arr1.length === arr2.length && 
+    arr1.every((value, index) => value === arr2[index]);
   
 }
 
 function getUsersNamesInAgeRange(users, gender) {
+
+  let filteredUsers = users.filter(user => user.gender === gender);
+  let filteredUsersAge = filteredUsers.map(filtereduser => filtereduser.age);
+
+  if (users.length != 0 && (gender === "мужской" || gender === "женский")) {
+    return filteredUsersAge.reduce((acc, item) => acc + item, 0) / filteredUsersAge.length;
+  } else {
+    return 0;
+  }
   
 }
